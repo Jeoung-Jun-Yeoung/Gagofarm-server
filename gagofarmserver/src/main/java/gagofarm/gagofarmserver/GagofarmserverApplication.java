14fd64370f0656fb2101b82b253ad7df341f6d2f
Jeoung-Jun-Yeoung <dbahsmssk@naver.com>
2022-10-05 01:19:17 +0900

    [ADD] : Spring Server create and Test

---
+++ gagofarmserver/src/main/java/gagofarm/gagofarmserver/GagofarmserverApplication.java
@@ -0,0 +1,12 @@
+package gagofarm.gagofarmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagofarmserverApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GagofarmserverApplication.class, args);
+	}
+
+}
---
+++ gagofarmserver/src/test/java/gagofarm/gagofarmserver/GagofarmserverApplicationTests.java
@@ -0,0 +1,13 @@
+package gagofarm.gagofarmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagofarmserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
